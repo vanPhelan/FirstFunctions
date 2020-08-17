0be9b0cfae8b50d95a89d69527f27f01e7c1cf67
Van Phelan <van.phelan@aie.edu>
2020-08-17 01:36:11 -0500

    Added game loop functions to Game
    Start(), Update(), and End() have been added to Game.

--- Game.cs
+++ Game.cs
@@ -6,9 +6,39 @@ namespace HelloWorld
 {
     class Game
     {
+        //Run the game
         public void Run()
+        {
+            //Perform initialization tasks
+            Start();
+            //Loop update routine until the game is over
+            while (!_gameOver)
+            {
+                Update();
+            }
+            //Perform cleanup tasks
+            End();
+        }
+
+        //Initialization method
+        public void Start()
         {
 
         }
+
+        //Method repeated each refresh
+        public void Update()
+        {
+
+        }
+
+        //Cleanup method
+        public void End()
+        {
+
+        }
+
+        public void ToggleGameOver() { _gameOver = !_gameOver; }
+        private bool _gameOver = false;
     }
 }
