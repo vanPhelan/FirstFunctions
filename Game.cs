4ad9cfc87eece3c6a347c67b0d73288358f33427
Van Phelan <van.phelan@aie.edu>
2020-08-30 15:57:54 -0500

    Removed parts to allow for additional practice

--- Game.cs
+++ Game.cs
@@ -6,39 +6,28 @@ namespace HelloWorld
 {
     class Game
     {
-        //Ends the game when true
-        private bool _gameOver = false;
-        
         //Run the game
         public void Run()
         {
-            //Perform initialization tasks
-            Start();
-            //Loop update routine until the game is over
-            while (!_gameOver)
-            {
-                Update();
-            }
-            //Perform cleanup tasks
-            End();
+            
         }
 
         //Performed once when the game begins
         public void Start()
         {
-
+            
         }
 
         //Repeated until the game ends
         public void Update()
         {
-
+            
         }
 
         //Performed once when the game ends
         public void End()
         {
-
+            
         }
     }
 }
